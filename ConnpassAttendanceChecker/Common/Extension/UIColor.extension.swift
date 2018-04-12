dc45ef70b0565799d02b7455e18957566e1db787
marty-suzuki <s1180183@gmail.com>
2018-04-12 12:35:32 +0900

    add detail information

---
+++ ConnpassAttendanceChecker/Common/Extension/UIColor.extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor.extension.swift
+//  ConnpassAttendanceChecker
+//
+//  Created by 鈴木大貴 on 2018/04/12.
+//  Copyright © 2018年 marty-suzuki. All rights reserved.
+//
+
+import Foundation
